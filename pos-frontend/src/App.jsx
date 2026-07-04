900a03d2c5c9d0c2f5d4931d51da1bf31577dbb6
Jazper Bustria <2235111@slu.edu.ph>
2026-07-04 12:14:48 +0800

    Initial progress on the project. Added basic routing and structure for the frontend application.

---
+++ pos-frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Home, Auth, Orders } from './pages'
+import Headers from './components/shared/Headers'
+function App() {
+
+  return (
+    <>
+      <Router>
+        <Headers />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/orders" element={<Orders />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ pos-frontend/src/components/shared/Headers.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { FaSearch, FaUserCircle, FaBell } from "react-icons/fa";
+import logo from '../../assets/images/logo.png'
+
+const Headers = () => {
+  return (
+
+    <header className ="flex justify-between items-center p-4 px-8 bg-gray-800 text-white">
+        {/*LOGO*/}
+        <div className="flex items-center gap-2">
+            <img src={logo} alt="Logo" className="h-8 w-auto" />
+            <h1 className="text-lg font-semibold text-white">POS</h1>
+        </div>
+        {/*SEARCH BAR*/}
+        <div className="flex items-center gap-4 bg-gray-700 rounded rounded-[15px] px-5 py-2 w-[500px]">
+            <FaSearch className="text-gray-400" />
+            <input
+                type="text"
+                placeholder="Search..."
+                className="bg-gray-700 outline-none text-white placeholder-gray-400"
+                
+                />
+        </div>
+        {/* Looged User */}
+        <div className="flex items-center gap-4">
+            <div className="bg-gray-700 rounded-[15px] p-3 cursor-pointer">
+                <FaBell className="text-white text-2xl" />
+            </div>
+            <div className="flex items-center gap-3 cursor-pointer">
+                <FaUserCircle className="text-white text-2xl" />
+                <div>
+                    <h1>Test User</h1>
+                    <p>Admin</p>
+                </div>
+            </div>
+        </div>
+
+
+    </header>
+  );
+};
+
+export default Headers;
